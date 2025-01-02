86f458da11c693e875a60441a5ae45f6abd79cdd
888Saint <psdas32@gmail.com>
2025-01-02 12:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+// Select the dropdown and button elements
+const colorSelect = document.getElementById('colorSelect');
+const button = document.querySelector('input[type="button"]');
+
+// Add click event listener to the button
+button.addEventListener('click', () => {
+    // Get the selected index
+    const selectedIndex = colorSelect.selectedIndex;
+
+    // Check if any option is selected
+    if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+    } else {
+        alert('Please select a color to remove.');
+    }
+});
